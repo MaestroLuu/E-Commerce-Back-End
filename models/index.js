47657c16541a9dca20e552274f6a087c099e8c73
David Luu <dluu.audeo@gmail.com>
2022-02-06 01:28:42 -0800

    revised productTag model to properly reference correct key, updated getAlls for product and tag routes so that associated data displays

--- models/index.js
+++ models/index.js
@@ -16,18 +16,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    foreignKey: 'category_id'
-  },
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    foreignKey: 'tag_id'
-  },
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
